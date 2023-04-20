f43d71b44002dfe66aa45fb5b25e485f75b25521
Firestone82 <mikpav@email.cz>
2023-04-20 23:35:46 +0200

    Fixed debug typo

--- src/main/java/cz/devfire/mysteryblocks/Placeholders/PlaceholderExpansion.java
+++ src/main/java/cz/devfire/mysteryblocks/Placeholders/PlaceholderExpansion.java
@@ -282,7 +282,7 @@ public class PlaceholderExpansion extends me.clip.placeholderapi.expansion.Place
                 }
             }
         } catch (Exception e) {
-            Bukkit.getConsoleSender().sendMessage("§4[FireMysteryBlocks-ERROR] &cUnknown placeholder: \"" + placeholder + "\"");
+            Bukkit.getConsoleSender().sendMessage("§4[FireMysteryBlocks-ERROR] §cUnknown placeholder: \"" + placeholder + "\"");
 
             if (MysteryBlocksPlugin.isDebugEnabled()) {
                 e.printStackTrace();
